977fae5c700c27b8a076fe7aa458f6b1da587fb4
mustapha jay <mustaphajay2@gmail.com>
2022-08-21 22:52:29 +0100

    added some content to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,24 @@
 const Home = () => {
   return (
-    <section>
-      <h1>home</h1>
+    <section className='text-white'>
+      <img
+        className="w-full"
+        src="https://media.istockphoto.com/photos/group-of-elementary-students-having-computer-class-with-their-teacher-picture-id1358014313?b=1&k=20&m=1358014313&s=170667a&w=0&h=t_bvZVk1eQhR1SiiuRphbJ3kljcJJ46QAsOQ8EnuLbA="
+        alt=""
+      />
+      <div className='p-[4rem] mission-img'>
+        <h1 className="text-center font-bold text-2xl my-5">Mission</h1>
+        <p className="text-center">
+          The mission of Amberschool is to develop intellectually curious and
+          capable young people who are selfless contributors to both local and
+          global communities, and who aspire to be ethical and compassionate
+          leaders. Through a rigorous and innovative academic program in math,
+          science, and technology, in combination with commerce and arts.
+          Amberschool affords its diverse student population those experiences,
+          skills, and values that will prepare them for purposeful contributions
+          in higher education and STEM-related fields.
+        </p>
+      </div>
     </section>
   );
 };
