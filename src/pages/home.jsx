8de68927f98117143a3ce9209261dfef64b79db9
mustapha jay <mustaphajay2@gmail.com>
2022-08-24 11:54:49 +0100

    edited styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,21 @@
 import { BsTrophy } from "react-icons/bs";
 import { GiBrain } from "react-icons/gi";
 import { FaGraduationCap } from "react-icons/fa";
+import image1 from "../assets/images/Rectangle 22.png";
+import image2 from "../assets/images/Rectangle 24.png";
+import image3 from "../assets/images/Rectangle 27.png";
 const Home = () => {
   return (
-    <section className="text-white">
+    <section className="">
+      {/* Hero */}
       <img
         className="w-full"
         src="https://media.istockphoto.com/photos/group-of-elementary-students-having-computer-class-with-their-teacher-picture-id1358014313?b=1&k=20&m=1358014313&s=170667a&w=0&h=t_bvZVk1eQhR1SiiuRphbJ3kljcJJ46QAsOQ8EnuLbA="
         alt=""
       />
-      <div className="p-[4rem] mission-img">
+
+      {/* Mission */}
+      <div className="p-[4rem] mission-img text-white">
         <h1 className="text-center font-bold text-2xl my-5">Mission</h1>
         <p className="text-center">
           The mission of Amberschool is to develop intellectually curious and
@@ -41,6 +47,8 @@ const Home = () => {
           <p className="text-3xl">Students</p>
         </div>
       </div>
+
+      {/* Programs */}
       <div className="md:grid grid-cols-3  gap-8 px-[4rem] py-[8rem] text-white">
         <div className="bg-[#0F3876] text-center p-[3rem] my-8 rounded-md flex flex-col items-center ">
           <h1 className="text-2xl font-bold p-4">Nursery</h1>
@@ -58,6 +66,23 @@ const Home = () => {
         <div className="bg-[#0F3876] text-center p-[3rem] my-8 rounded-md flex flex-col items-center ">
           <h1 className="text-2xl font-bold p-4">Secondary</h1>
           <h1 className="text-lg">JSS1-SS3</h1>
+        </div>
+      </div>
+
+      {/* Gallery & News */}
+      <div className="flex justify-between mx-[3.5rem] gap-4 text-black">
+        <div className="">
+          <h1 className="font-semibold text-2xl mb-8 capitalize">
+            graduation class galleria
+          </h1>
+          <div className="grid grid-rows-2">
+            <img src={image1} className="row-span-full" alt="" />
+            <img src={image2} className="row-start-1 " alt="" />
+            <img src={image3} className="" alt="" />
+          </div>
+        </div>
+        <div className="">
+          <h1 className="font-semibold text-2xl mb-8 capitalize">newsroom</h1>
         </div>
       </div>
     </section>
