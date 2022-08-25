77874d4f6e573da718a1f65990a5d5030560ed99
mustapha jay <mustaphajay2@gmail.com>
2022-08-25 14:00:21 +0100

    added gallery and news section and updated some texts and style

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,8 @@ import { FaGraduationCap } from "react-icons/fa";
 import image1 from "../assets/images/Rectangle 22.png";
 import image2 from "../assets/images/Rectangle 24.png";
 import image3 from "../assets/images/Rectangle 27.png";
+import image28 from "../assets/images/Rectangle 28.png";
+import image30 from "../assets/images/Rectangle 30.png";
 const Home = () => {
   return (
     <section className="">
@@ -19,32 +21,32 @@ const Home = () => {
         <h1 className="text-center font-bold text-2xl my-5">Mission</h1>
         <p className="text-center">
           The mission of Amberschool is to develop intellectually curious and
-          capable young people who are selfless contributors to both local and 
-          global communities, and who aspire to be ethical and compassionate 
+          capable young people who are selfless contributors to both local and
+          global communities, and who aspire to be ethical and compassionate
           leaders. Through a rigorous and innovative academic program in math,
-          science, and technology, in combination with commerce and arts. 
+          science, and technology, in combination with commerce and arts.
           Amberschool affords its diverse student population those experiences,
           skills, and values that will prepare them for purposeful contributions
           in higher education and STEM-related fields.
         </p>
       </div>
-      <div className="md:flex justify-between bg-[#F0F0F0] px-[4rem] py-[8rem] text-black text-6xl">
+      <div className="sm:flex justify-between bg-[#F0F0F0] px-[6rem] py-[8rem] text-black text-5xl">
         <div className="flex flex-col items-center my-[3rem]">
           <FaGraduationCap className="text-orange-500" />
-          <h1 className="text-orange-500 text-4xl my-3">#5</h1>
-          <p className="text-3xl">Ranked</p>
+          <h1 className="text-orange-500 text-2xl my-3">#5</h1>
+          <p className="text-lg">Ranked</p>
         </div>
         <div className="flex flex-col items-center my-[3rem]">
           <BsTrophy className="text-orange-500" />
 
-          <h1 className="text-orange-500 text-4xl my-3">15+</h1>
-          <p className="text-3xl">School Programs</p>
+          <h1 className="text-orange-500 text-2xl my-3">15+</h1>
+          <p className="text-lg">School Programs</p>
         </div>
         <div className="flex flex-col items-center my-[3rem]">
           <GiBrain className="text-orange-500" />
 
-          <h1 className="text-orange-500 text-4xl my-3">#5</h1>
-          <p className="text-3xl">Students</p>
+          <h1 className="text-orange-500 text-2xl my-3">#5</h1>
+          <p className="text-lg">Students</p>
         </div>
       </div>
 
@@ -70,7 +72,7 @@ const Home = () => {
       </div>
 
       {/* Gallery & News */}
-      <div className="flex justify-between mx-[3.5rem] gap-4 text-black">
+      <div className="flex mx-[3.5rem] gap-6 text-black">
         <div className="">
           <h1 className="font-semibold text-2xl mb-8 capitalize">
             graduation class galleria
@@ -80,9 +82,38 @@ const Home = () => {
             <img src={image2} className="row-start-1 " alt="" />
             <img src={image3} className="" alt="" />
           </div>
+          <div className="grid grid-rows-2">
+            <img src={image28} className="" alt="" />
+            <img src={image30} className=" col-start-2" alt="" />
+          </div>
         </div>
-        <div className="">
-          <h1 className="font-semibold text-2xl mb-8 capitalize">newsroom</h1>
+        <div className="w-[40%] mx-auto">
+          <h1 className="font-semibold text-2xl mb-8 capitalize text-left">
+            newsroom
+          </h1>
+          <div>
+            <hr className="mb-4 bg-black h-[2px]" />
+            <h1 className="font-bold capitalize">
+              amberschool school of 2022 graduation list
+            </h1>
+            <p className="font-semibold mb-8">12/03/2023</p>
+            <hr className="mb-4 bg-black h-[2px]" />
+            <h1 className="font-bold capitalize">
+              amberschool school of 2022 graduation list
+            </h1>
+            <p className="font-semibold mb-8">12/03/2023</p>
+            <hr className="mb-4 bg-black h-[2px]" />
+            <h1 className="font-bold capitalize">
+              amberschool school of 2022 graduation list
+            </h1>
+            <p className="font-semibold mb-8">12/03/2023</p>
+            <hr className="mb-4 bg-black h-[2px]" />
+            <h1 className="font-bold capitalize">
+              amberschool school of 2022 graduation list
+            </h1>
+            <p className="font-semibold mb-8">12/03/2023</p>
+          </div>
+          <button className="px-4 py-1 border-orange-500 border-2 border-solid text-orange-500 rounded-sm">More News</button>
         </div>
       </div>
     </section>
