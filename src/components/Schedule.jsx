959d7bf5c06845d1691816d44623e84d3df0ddae
mustapha jay <mustaphajay2@gmail.com>
2022-08-31 14:58:19 +0100

    added calendar component

---
+++ src/components/Schedule.jsx
@@ -0,0 +1,15 @@
+import Calendar from 'react-calendar';
+import {useState} from 'react'
+import "react-calendar/dist/Calendar.css";
+
+const Schedule = () => {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
+
+export default Schedule
