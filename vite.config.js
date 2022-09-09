f4bef6add2ed5df82793fdf929429c7a70c7d958
mustapha jay <mustaphajay2@gmail.com>
2022-09-09 23:52:39 +0100

    removed base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: "/school-cms/",
-  plugins: [react()]
-})
+  // base: "/school-cms/",
+  plugins: [react()],
+});
