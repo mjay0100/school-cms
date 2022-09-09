aa09624be867587016f957f3aea42f45669ae369
mustapha jay <mustaphajay2@gmail.com>
2022-09-09 14:47:03 +0100

    fixed pages

--- src/pages/Academics.jsx
+++ src/pages/Academics.jsx
@@ -1,98 +1,109 @@
+import science from "../assets/images/science.png";
+import mayor from "../assets/images/mayor.png";
+import strategy from "../assets/images/strategy.png";
+
 const Academics = () => {
   return (
     <div>
-      <div className="acad-container relative z-[-1] ">
-        <h3 className="about-text text-white text-4xl absolute text-center top-[50%] w-full ">Academics</h3>
+      <div className="academic-container relative z-[-1] ">
+        <h3 className="text-center text-white text-4xl absolute text-center top-[50%] w-full ">
+          Academics
+        </h3>
       </div>
-      <h3 className="text-2xl text-center py-24 ">
-        At Amber Academy our students success is rooted in the future. Students
-        master <br />
-        academic and real-world skills today, that will be in demand when they
-        graduate. <br />
-        <br />
-        Learn more about the school that is setting the standard for education.
-      </h3>
-
-      <div className="md:flex md:flex-row align-middle justify-center bg-[#f0f0f0]">
-        <div className="md:basis-1/2 py-5">
-          <img src="src/assets/images/Rectangle 112.png" width="500px" />
-        </div>
-
-        <div className="pt-12">
-          <h3 className="text-3xl  font-bold ">Elementary School</h3>
-          <p className="text-xl mt-2">Nursery 1 & 2</p>
-
-          <p className="text-lg mt-7 text-left">
-            Young minds are like newly formed galaxies. Full of energy, <br />
-            expansive & curious. Poised & ready to stretch out and make <br />
-            their mark in the universe. At Amber academy, we believe that <br />
-            every child is full of brilliance and potential. At Amber academy{" "}
-            <br />
-            Elementary, they discover it for themselves.
-          </p>
-        </div>
+      <div className="bg-[#f0f0f0]">
+        <h3 className="text-xl text-center px-8 py-20 w-[90%] mx-auto rounded-md ">
+          At Amber Academy our students success is rooted in the future.
+          Students master academic and real-world skills today, that will be in
+          demand when they graduate.
+          <br />
+          Learn more about the school that is setting the standard for
+          education.
+        </h3>
       </div>
 
-      <div className="md:flex md:flex-row-reverse align-middle justify-center bg-[#d3b2b2] pt-5">
-        <div className=" py-5">
-          <img src="src/assets/images/abc.png" width="500px" />
+      {/* sections */}
+      <section className="my-6">
+        <div className="md:flex items-center justify-center py-8 px-10 gap-5 ">
+          <div className="py-5">
+            <img
+              src="src/assets/images/Rectangle 112.png"
+              className="mx-auto rounded-md"
+            />
+          </div>
+          <div className="md:w-1/2 p-8 ">
+            <h3 className="text-2xl mb-2 font-bold text-center ">
+              Elementary School
+            </h3>
+            <p className="text-xl mb-4 text-center">Nursery 1 & 2</p>
+            <p className="text-lg text-center">
+              Young minds are like newly formed galaxies. Full of energy,
+              expansive & curious. Poised & ready to stretch out and make their
+              mark in the universe. At Amber academy, we believe that every
+              child is full of brilliance and potential. At Amber academy
+              Elementary, they discover it for themselves.
+            </p>
+          </div>
         </div>
 
-        <div className="basis-1/2 pt-12 pl-20">
-          <h3 className="text-3xl  font-bold ">Primary School</h3>
-          <p className="text-xl mt-2 ">Primary 1 & 6</p>
-
-          <p className="text-lg mt-10 text-left ">
-            Here at Amber Academy we envision every student becoming a <br />
-            world-class scientist, technologist, engineer or mathematician.{" "}
-            <br />
-            We encourage students to become critical thinkers & to explore{" "}
-            <br />
-            and experiment with their talents & discover their purpose. <br />
-          </p>
-        </div>
-      </div>
-
-      <div className="md:flex md:flex-row align-middle justify-center bg-[#f0f0f0] pt-5">
-        <div className="basis-1/2 py-5">
-          <img
-            src="https://www.brookings.edu/wp-content/uploads/2019/11/CUE_South-Africa_classroom001.jpg"
-            width="500px"
-          />
+        <div className="md:flex md:flex-row-reverse items-center justify-center pb-8 px-10 gap-5">
+          <div className=" py-5">
+            <img
+              src="src/assets/images/abc.png"
+              className="mx-auto rounded-md"
+            />
+          </div>
+          <div className="md:w-1/2 p-8 ">
+            <h3 className="text-2xl mb-2 font-bold text-center ">
+              Primary School
+            </h3>
+            <p className="text-xl mb-4 text-center ">Primary 1 & 6</p>
+            <p className="text-lg text-center ">
+              Here at Amber Academy we envision every student becoming a <br />
+              world-class scientist, technologist, engineer or mathematician. We
+              encourage students to become critical thinkers & to explore and
+              experiment with their talents & discover their purpose. <br />
+            </p>
+          </div>
         </div>
 
-        <div className="pt-12">
-          <h3 className="text-3xl  font-bold ">Secondary School</h3>
-          <p className="text-xl mt-2">JS1 - SS3</p>
-
-          <p className="text-lg mt-10 text-left">
-            Young minds are like newly formed galaxies. Full of energy, <br />
-            expansive & curious. Poised & ready to stretch out and make <br />
-            their mark in the universe. At Amber academy, we believe that <br />
-            every child is full of brilliance and potential. At Amber academy{" "}
-            <br />
-            Elementary, they discover it for themselves.
-          </p>
+        <div className="md:flex items-center justify-center pb-8 px-10 gap-5 ">
+          <div className="basis-1/2 py-5">
+            <img src="https://www.brookings.edu/wp-content/uploads/2019/11/CUE_South-Africa_classroom001.jpg" />
+          </div>
+          <div className="md:w-1/2 p-8">
+            <h3 className="text-2xl mb-2 font-bold text-center">
+              Secondary School
+            </h3>
+            <p className="text-xl mt-2 text-center">JS1 - SS3</p>
+            <p className="text-lg text-center ">
+              Young minds are like newly formed galaxies. Full of energy, <br />
+              expansive & curious. Poised & ready to stretch out and make <br />
+              their mark in the universe. At Amber academy, we believe that{" "}
+              <br />
+              every child is full of brilliance and potential. At Amber academy{" "}
+              Elementary, they discover it for themselves.
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
+
+      {/* our fields */}
 
       <div className=" my-10">
-        <h3 className=" text-center text-3xl text-black font-bold">
-          Our Fields
-        </h3>
+        <h3 className=" text-center text-4xl font-bold">Our Fields</h3>
 
-        <div className="flex flex-col mx-32">
-          <div className="basis-1 bg-[#0f3768] my-2 py-3 text-3xl text-white pl-2">
-            {" "}
-            Science & Technology
+        <div className="w-2/3 mx-auto grid md:grid-cols-3 gap-5 items-center">
+          <div className=" my-2 p-3 text-xl  rounded-md items-center">
+            <img src={science} alt="science" className="w-20 mx-auto mb-3" />
+            <p className="text-center text-[#0f3768]">Science & Technology</p>
           </div>
-          <div className="basis-1 bg-[#0f3768] my-2 py-3 text-3xl text-white pl-2">
-            {" "}
-            Art & Humanities
+          <div className=" my-2 p-3 text-xl  rounded-md items-center">
+            <img src={mayor} alt="arts" className="w-20 mx-auto mb-3" />
+            <p className="text-center text-[#0f3768]"> Art & Humanities</p>
           </div>
-          <div className="basis-1 bg-[#0f3768] my-2 py-3 text-3xl text-white pl-2">
-            {" "}
-            Commercial
+          <div className=" my-2 p-3 text-xl  rounded-md items-center">
+            <img src={strategy} alt="commerce" className="w-20 mx-auto mb-3" />
+            <p className="text-center text-[#0f3768]"> Commercial</p>
           </div>
         </div>
       </div>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,133 +3,105 @@ const About = () => {
     <section>
       <div className=" first-container relative z-[-1] ">
         {/* <img className = "w-full " src = "src/images/photo school kid100.jpg" style={{color:red}}/> */}
-        <h3 className="about-text text-white text-4xl absolute text-center top-[50%] w-full ">
+        <h3 className="text-center text-white text-4xl absolute text-center top-[50%] w-full ">
           About Us
         </h3>
       </div>
+      {/* About school */}
+      <article className="bg-[#fb6f00] p-5">
+        <div className="mx-auto py-4">
+          <h3 className="text-3xl text-center text-white pt-10">
+            About Amber Academy
+          </h3>
 
-      <div className="second-container ">
-        <h3 className="text-4xl text-center text-white pt-10">About Amber Academy</h3>
+          <p className="text-center w-[90%] mx-auto text-white py-6">
+            The mission of Amberschool is to develop intellectually curious and
+            capable young people who are selfless contributors to both local and
+            global communities, and who aspire to be ethical and compassionate
+            leaders. Through a rigorous and innovative academic program in math,
+            science, and technology, in combination with the liberal arts,
+            Amberschool affords its diverse student population those
+            experiences, skills, and values that will prepare them for
+            purposeful contributions in higher education and STEM-related
+            fields.
+          </p>
+        </div>
+      </article>
 
-        <p className="text-center text-xl  text-white break-normal py-6">
-          The mission of Amberschool is to develop intellectually curious and
-          capable young people who are selfless <br />
-          contributors to both local and global communities, and who aspire to
-          be ethical and compassionate leaders. <br />
-          Through a rigorous and innovative academic program in math, science,
-          and technology, <br />
-          in combination with the liberal arts, Amberschool affords its diverse
-          student population <br />
-          those experiences, skills, and values that will prepare them for
-          purposeful contributions in
-          <br />
-          higher education and STEM-related fields.
-        </p>
-      </div>
-      {/* 
-        <div className = "third-container">
-          <h3 className = "py-10 text-4xl text-center text-bold">Mission&Vision</h3>
-          <p className = "text-lg pb-4 text-center ">
-          The mission of Amberschool  is to develop intellectually curious and capable young people who are selfless <br />
-           contributors to both local and global communities, and who aspire to be ethical and compassionate leaders. <br />
-            Through a rigorous and innovative academic program in math, science, and technology, in combination with the <br />
-             liberal arts, Amberschool affords its diverse student population those experiences, skills, and values that will <br />
-              prepare them for purposeful contributions in higher education and STEM-related fields. */}
+      {/* Mission and Vision */}
+      <article className="bg-[#f0f0f0] p-5">
+        <div className="mx-auto w-[90%] py-4">
+          <h3 className="text-3xl text-center text-black pt-10">
+            Mission & Vision
+          </h3>
+          <p className="text-center text-black py-6">
+            The mission of Amberschool is to develop intellectually curious and
+            capable young people who are selfless contributors to both local and
+            global communities, and who aspire to be ethical and compassionate
+            leaders. Through a rigorous and innovative academic program in math,
+            science, and technology, in combination with the liberal arts,
+            Amberschool affords its diverse student population those
+            experiences, skills, and values that will prepare them for
+            purposeful contributions in higher education and STEM-related
+            fields. The sole vision of Amberschool is to create an environment
+            with highly intellectual and skilled young people who will be able
+            to handle and control negative impulses, be intellectually fit and
+            morally upright.
+          </p>
+        </div>
+      </article>
 
-      <div className="third-container">
-        <h3 className="text-4xl text-center text-black pt-10">Mission&Vision</h3>
-        <p className="text-center text-xl  text-black break-normal py-6">
-          The mission of Amberschool is to develop intellectually curious and
-          capable young people who are selfless <br />
-          contributors to both local and global communities, and who aspire to
-          be ethical and compassionate leaders. <br />
-          Through a rigorous and innovative academic program in math, science,
-          and technology, in combination with the <br />
-          liberal arts, Amberschool affords its diverse student population those
-          experiences, skills, and values that will <br />
-          prepare them for purposeful contributions in higher education and
-          STEM-related fields.
-          <br />
-          <br />
-          The sole vision of Amberschool is to create an environment with highly
-          intellectual and skilled young people <br />
-          who will be able to handle and control negative impulses, be
-          intellectually fit and morally upright.
-        </p>
-      </div>
+      {/* core values */}
+      <article className=" p-10">
+        <h3 className="py-8 text-4xl text-center text-bold">Core Values</h3>
 
-      <div className="fourth-container bg-white  pb-14">
-        <h3 className="py-10 text-4xl text-center text-bold">Core Values</h3>
-
-        <div className="flex flex-row gap-10 justify-between px-20 md:mx-24 ">
-          <div className="basis-1/1 ">
-            <h4 className="text-2xl md:text-3xl ">
-              Fostering Academic Excellence
-            </h4>
-            <p className=" mt-4 text-base md:text-lg">
-              By supporting skilled and dedicated teachers who <br />
-              maintain high academic standards, motivate our <br />
-              students and collaborate with our families
+        <div className="grid md:grid-cols-2 gap-7 w-[85%] mx-auto text-center">
+          <div>
+            <h4 className="text-2xl mb-4">Fostering Academic Excellence</h4>
+            <p>
+              By supporting skilled and dedicated teachers who maintain high
+              academic standards, motivate our students and collaborate with our
+              families
             </p>
           </div>
 
-          <div className="basis-1/1">
-            <h4 className="text-2xl md:text-3xl">Nurturing The Whole Child</h4>
-            <p className="mt-4  text-base md:text-lg">
-              By creating engaged learners, encouraging <br />
-              honesty and guiding social and emotional <br />
-              development
+          <div>
+            <h4 className="text-2xl mb-4">Nurturing The Whole Child</h4>
+            <p>
+              By creating engaged learners, encouraging honesty and guiding
+              social and emotional development
+            </p>
+          </div>
+          <div>
+            <h4 className="text-2xl mb-4">Ensuring A Safe Environment</h4>
+            <p>
+              By supporting skilled and dedicated teachers who maintain high
+              academic standards, motivate our students and collaborate with our
+              families
+            </p>
+          </div>
+
+          <div>
+            <h4 className="text-2xl mb-4">Developing Critical Thinkers</h4>
+            <p className=" ">
+              By encouraging intellectual curiosity and rewarding independent
+              and creative problem solvers
             </p>
           </div>
         </div>
+      </article>
 
-        <div className="flex flex-row gap-10 justify-between px-20 md:mx-24 ">
-          <div className="basis-1/1 mt-10">
-            <h4 className="text-2xl md:text-3xl">
-              Ensuring A Safe Environment
-            </h4>
-            <p className="mt-4 text-base md:text-lg ">
-              By supporting skilled and dedicated teachers who <br />
-              maintain high academic standards, motivate our <br />
-              students and collaborate with our families
-            </p>
-          </div>
-
-          <div className="basis-1/1 mt-10">
-            <h4 class="text-2xl md:text-3xl">Developing Critical Thinkers</h4>
-            <p className="mt-4 text-base md:text-lg">
-              By creating engaged learners, encouraging <br />
-              honesty and guiding social and emotional <br />
-              development
-            </p>
-          </div>
+      {/* partners */}
+      <div className="bg-[#f0f0f0] p-8">
+        <h3 className="text-3xl text-center pb-9">Partners</h3>
+        <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-6 mx-auto w-9/12 md:w-4/5  pb-10 ">
+          <div className="partner"></div>
+          <div className="partner"></div>
+          <div className="partner"></div>
+          <div className="partner"></div>
+          <div className="partner"></div>
+          <div className="partner"></div>
         </div>
-      </div>
-
-      <div className="fifth-container third-container">
-        <h3 className="third-container-text100">Partners</h3>
-
-        <div className="lg:flex px-10  ">
-          <div className="  flex gap-3  pb-10 ">
-            <div className=" md:grid-cols-1 lil-box"></div>
-
-            <div className=" md:grid-cols-1 lil-box "></div>
-          </div>
-
-          <div className="  gap-3 flex  pb-10 lg:mx-3">
-            <div className=" md:grid-cols-1 lil-box"></div>
-
-            <div className=" md:grid-cols-1 lil-box "></div>
-          </div>
-
-          <div className="gap-3 flex pb-10">
-            <div className="  md:grid-cols-1 lil-box "></div>
-
-            <div className=" md:grid-cols-1 lil-box "></div>
-          </div>
-        </div>
-
-
       </div>
     </section>
   );
