2d3029015c86ed5e71c5ef6ca9ff85fb2e545cde
mustapha jay <mustaphajay2@gmail.com>
2022-09-09 16:17:23 +0100

    added aos effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import Route from "./router/Index";
+import { useEffect } from "react";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration: 700,
+      mirror: true,
+    });
+    Aos.refresh();
+  }, []);
   return <Route />;
 }
 
